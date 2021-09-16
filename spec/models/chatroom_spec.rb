e626ae5b977d0e1da0785a556ffe9f6a30b05f5d
Henok <henatan99@gmail.com>
2021-09-16 19:40:16 +0300

    Add spec tests got Chatroom model associations and validations

--- spec/models/chatroom_spec.rb
+++ spec/models/chatroom_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Chatroom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:messages) }
+
+  it { should validate_presence_of(:name) }
 end
