fd7029c2c4c02e3619bfe390a8b6f930083ab1d9
Henok <henatan99@gmail.com>
2021-09-16 19:41:52 +0300

    Add spec tests to Message model associations and validations

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:chatroom) }
+
+  it { should validate_presence_of(:content) }
 end
