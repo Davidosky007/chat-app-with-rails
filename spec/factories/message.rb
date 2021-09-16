2d309e0269791cea4ffbd80a2a14dcc7df61d851
Henok <henatan99@gmail.com>
2021-09-16 20:01:39 +0300

    Add factories for models

---
+++ spec/factories/message.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :message do
+      content { Faker::Lorem.word }
+      user_id nil
+      chatroom_id nil
+    end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+    factory :user do
+      username { Faker::Name.name }
+    end
+end
