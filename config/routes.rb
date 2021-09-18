e7193bde495b996113c0a9a30a0391472eea840a
yocosaka <sakayoco55@gmail.com>
2021-09-18 17:12:11 +0900

    Add routes and update ERD

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      # resources :items, except: %i[new]
-      # resources :users, only: %i[show create]
+      resources :users, only: %i[show create]
+      resources :chatrooms, only: %i[show create] do
+        resources :messages, only: %i[show create]
+      end
       # resources :login, only: %i[create]
       # resources :auto_login, only: %i[create]
-      # resources :records, except: %i[new]
     end
   end
 end
