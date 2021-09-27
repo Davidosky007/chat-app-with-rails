d34a3b4d4127e1dcb6191a89b648a57075e52cad
Henok <henatan99@gmail.com>
2021-09-27 14:41:10 +0300

    Change database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_18_031738) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -20,8 +23,8 @@ ActiveRecord::Schema.define(version: 2021_09_18_031738) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
-    t.integer "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "chatroom_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
